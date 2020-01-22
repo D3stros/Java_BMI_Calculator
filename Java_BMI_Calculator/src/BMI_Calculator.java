ea00d66465d546035ee63c375571f71fa5fc60f6
D3stros <patrick.fallmann@hotmail.com>
2020-01-22 19:52:04 +0100

    Created a Java BMI calculator

--- Java_BMI_Calculator/src/BMI_Calculator.java
+++ Java_BMI_Calculator/src/BMI_Calculator.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class BMI_Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter height in m (e.g. 1,75 or 1,80)");
+		double height = sc.nextDouble(); // Read weight
+		
+		System.out.println("Enter weight in kgs");
+		double weight = sc.nextDouble(); // Read weight
+		
+		// Calculating BMI
+		double bmi = weight/(height*height);
+		
+		System.out.println("Your BMI is " + bmi);
 	}
 
 }
